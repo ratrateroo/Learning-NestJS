8e73118a8566c72fc3d5b2f31245450932cf059f
Mark_Leo <ratrateroo@gmail.com>
2022-08-02 21:39:55 +0800

    added endport for single product

--- nest-practice/src/products/products.service.ts
+++ nest-practice/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Product } from './product.model';
 
@@ -7,7 +7,7 @@ export class ProductsService {
   private products: Product[] = [];
   //insertProduct(prod: Product)
   insertProduct(title: string, desc: string, price: number) {
-    const prodId = new Date().toString();
+    const prodId = Math.random().toString();
     const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
     return prodId;
@@ -15,5 +15,13 @@ export class ProductsService {
 
   getProducts() {
     return [...this.products];
+  }
+
+  getSingleProduct(productId: string) {
+    const product = this.products.find((prod) => prod.id === productId);
+    if (!product) {
+      throw new NotFoundException('Cound not find product.');
+    }
+    return { ...product };
   }
 }
